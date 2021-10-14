3c3ae9471d2a33188fbc6981f35a724c12d0c388
João <92548291+j0n1s@users.noreply.github.com>
2021-10-14 18:43:54 -0300

    Add files via upload

---
+++ Calc + -.py
@@ -0,0 +1,9 @@
+n1 = int(input('1st number '))
+operation = input('+ or - ')
+n2 = int(input('2nd number '))
+if operation == "+":
+    print(n1+n2)
+if operation == "-":
+    print ("the result is",n1-n2)
+
+
